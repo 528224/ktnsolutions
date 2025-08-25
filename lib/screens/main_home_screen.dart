54ccccdc08f57ae7220ea164876a4d5b71529548
simjo <simjotr.ec@gmail.com>
2025-08-25 12:31:27 +0530

    updated ui to mtch with google serch

--- lib/screens/main_home_screen.dart
+++ lib/screens/main_home_screen.dart
@@ -21,6 +21,7 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
   ];
 
   void _onItemTapped(int index) {
+    if(index != 0) return;//TODO need to implement
     setState(() {
       _selectedIndex = index;
     });
